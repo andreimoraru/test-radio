315340c8fb86d9b924a2c061c50c53844fa95c30
andreimoraru <andrei@moraru.online>
2021-09-10 22:07:24 +0300

    Update jukebox-card-test.js

--- jukebox-card-test.js
+++ jukebox-card-test.js
@@ -188,7 +188,7 @@ class JukeboxCardTest extends HTMLElement {
         setTimeout(function() {
             this.hass.callService('media_player', 'media_play', {
                 entity_id: this._selectedSpeaker});
-            }, 5000);
+            }, 10000);
     }
 
     setVolume(value) {
