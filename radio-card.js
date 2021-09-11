d95411bf50b7a9109ea964f097e732ff52c522d4
Andrei <andrei@moraru.online>
2021-09-12 00:27:56 +0300

    added repeated play with timeout

--- radio-card.js
+++ radio-card.js
@@ -192,12 +192,12 @@ class JukeboxCardTest extends HTMLElement {
                 title: e.currentTarget.innerText
             }
         });
-        /*  setTimeout(function() {
-        //     this.hass.callService('media_player', 'media_play', {
-        //         entity_id: this._selectedSpeaker});
-             }, 5000); */
-        this.hass.callService('media_player', 'media_play', {
-            entity_id: this._selectedSpeaker});
+          setTimeout(function() {
+             this.hass.callService('media_player', 'media_play', {
+                 entity_id: this._selectedSpeaker});
+                }, 5000);
+        /* this.hass.callService('media_player', 'media_play', {
+            entity_id: this._selectedSpeaker});*/
     }
 
     setVolume(value) {
