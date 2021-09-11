71c8dc9ef8bfa350588292ab5359742d9f5d9a70
Andrei <andrei@moraru.online>
2021-09-12 01:11:29 +0300

    some debugging

--- radio-card.js
+++ radio-card.js
@@ -196,17 +196,28 @@ class JukeboxCardTest extends HTMLElement {
              this.hass.callService('media_player', 'media_play', {
                  entity_id: this._selectedSpeaker});
                 }, 3000);*/
-        while (this.hass.states[this._selectedSpeaker].state !== 'playing') {
+            console.log(this.hass.states[this._selectedSpeaker].state);
+            setTimeout(function () { console.log("Wait!") }, 1000);
+            console.log(this.hass.states[this._selectedSpeaker].state);
+            setTimeout(function () { console.log("Wait!") }, 1000);
+            console.log(this.hass.states[this._selectedSpeaker].state);
+            setTimeout(function () { console.log("Wait!") }, 1000);
+            console.log(this.hass.states[this._selectedSpeaker].state);
+            setTimeout(function () { console.log("Wait!") }, 1000);
+            console.log(this.hass.states[this._selectedSpeaker].state);
+            setTimeout(function () { console.log("Wait!") }, 1000);
+        this.hass.callService('media_player', 'media_play', {
+            entity_id: this._selectedSpeaker
+        });
+        /*while (this.hass.states[this._selectedSpeaker].state !== 'playing') {
             this.hass.callService('media_player', 'media_play', {
                 entity_id: this._selectedSpeaker
             });
-            for (let i = 1; i <= 15; i++) {
-                setTimeout(() => console.log(`waiting #${i}`), 1000 * i)
-              }
+            setTimeout(function () { console.log("Wait!") }, 1000)
             if(this.hass.states[this._selectedSpeaker].state === 'playing') {
                 break;
             }
-        }
+        }*/
     }
 
     setVolume(value) {
